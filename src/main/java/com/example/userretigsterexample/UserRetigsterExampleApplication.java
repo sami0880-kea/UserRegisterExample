3780c9b2cf78b3a01fc5c4cfc7828382988b8177
Samim <sami0880@stud.kea.dk>
2023-03-16 14:08:17 +0100

    Created User Registration

---
+++ src/main/java/com/example/userretigsterexample/UserRetigsterExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.userretigsterexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRetigsterExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserRetigsterExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/userretigsterexample/UserRetigsterExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userretigsterexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRetigsterExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
